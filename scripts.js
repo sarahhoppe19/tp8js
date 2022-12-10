124c1f0ba965762ba8d85851c5235b172d133fb9
sarahhoppe19 <sarah.hoppe@wsu.edu>
2022-12-10 23:37:50 +0000

    added images and basic clicks for flipping

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
 //java script code for memory game - TP8JS
 
+window.onload = function() {
+  cardList = document.getElementsByClassName("card"); //collection of cards
+  
+  cardCount = cardList.length; //how many cards are on the board
+  
+  for (i = 0; i < cardCount; i++) {
+    cardList[i].onclick = function() {
+      this.classList.toggle("clicked");
+    }
+  }
+  
+  
+}
